09d0e4eb00b220a4bd6148750cb36cb3808e4e31
dz333 <dzag@cs.cornell.edu>
2020-09-11 17:59:50 -0400

    minor testing file changes

--- testOutputs/runbsc.sh
+++ testOutputs/runbsc.sh
@@ -3,7 +3,7 @@
 ARGS="-no-show-timestamps -no-show-version --aggressive-conditions"
 BPATH="-p .:"$(realpath ~/volume/SpecLang/bscRuntime/locks)":"$(realpath ~/volume/SpecLang/bscRuntime/memories)":"$BLUESPECDIR/inst/lib/Libraries/
 #Compiling Base Module
-bsc $ARGS $BPATH -show-schedule -verilog CPU.bsv
+bsc $ARGS $BPATH -show-schedule -verilog Cpu.bsv
 #Compile test bench
 bsc $ARGS $BPATH -show-schedule -verilog tb.bsv
 #Run simulation for like...a million cycles
