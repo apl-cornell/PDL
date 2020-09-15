7c0c397e4841654195d34409925d047ea1941cbe
dz333 <dz333>
2020-09-15 14:45:56 -0400

    Continuing work on if elimination - progressing well

--- src/main/scala/pipedsl/passes/CollapseStagesPass.scala
+++ src/main/scala/pipedsl/passes/CollapseStagesPass.scala
@@ -18,50 +18,29 @@ import pipedsl.passes.Passes.StagePass
 object CollapseStagesPass extends StagePass[List[PStage]] {
 
   override def run(stgs: List[PStage]): List[PStage] = {
-    stgs.map(s => mergeStage(s))
+    stgs.map(s => eliminateIfs(s))
   }
 
-  private def mergeStage(stg: PStage): PStage = stg match {
+  private def eliminateIfs(stg: PStage): PStage = stg match {
     case s: IfStage =>
       //TODO recursively merge inner portions of the pipeline
+      //Check if the branches are combinational
       val isTrueComb = s.trueStages.head == s.trueStages.last
       val isFalseComb = s.falseStages.head == s.falseStages.last
+      //Merge in the first true and false stages since that delay is artificial
+      mergeStages(s, s.trueStages.head)
+      mergeStages(s, s.falseStages.head)
+      //Update IF stage metadata
+      //TODO convert this to a normal PStage so we can eliminate this extra bookkeeping
+      s.trueStages = s.trueStages.tail
+      s.falseStages = s.falseStages.tail
+      //Get the inputs to the join stage from any conditional edge
       val joinStgInputs = s.joinStage.inEdges.filter(e => e.condRecv.isDefined).head.values
-      (isTrueComb, isFalseComb) match {
-        case (false, false) => ()
-        case (false, true) => {
-          addCondStmts(s, s.notCond, s.falseStages.head.cmds)
-          //conditionally send and receive on this edge directly from the IF beginning to the Join stage
-          //replacing the old edges to the FALSE branch and join stage
-          val newOutEdge = PipelineEdge(Some(s.notCond), Some(s.notCond), s, s.joinStage, joinStgInputs)
-          s.removeEdgesTo(s.falseStages.head)
-          s.addEdge(newOutEdge)
-          s.falseStages.head.removeEdgesTo(s.joinStage)
-          s.falseStages = List()
-        }
-        case (true, false) => {
-          addCondStmts(s, s.condVar, s.trueStages.head.cmds)
-          //conditionally send and receive on this edge directly from the IF beginning to the Join stage
-          //replacing the old edges to the TRUE branch and join stage
-          val newOutEdge = PipelineEdge(Some(s.condVar), Some(s.condVar), s, s.joinStage,joinStgInputs)
-          s.removeEdgesTo(s.trueStages.head)
-          s.addEdge(newOutEdge)
-          s.trueStages.head.removeEdgesTo(s.joinStage)
-          s.trueStages = List()
-        }
-        case (true, true) => {
-          addCondStmts(s, s.condVar, s.trueStages.head.cmds)
-          addCondStmts(s, s.notCond, s.falseStages.head.cmds)
-          val newOutEdge = PipelineEdge(None, None, s, s.joinStage, joinStgInputs)
-          s.removeEdgesTo(s.joinStage)
-          s.removeEdgesTo(s.trueStages.head)
-          s.removeEdgesTo(s.falseStages.head)
-          s.falseStages.head.removeEdgesTo(s.joinStage)
-          s.trueStages.head.removeEdgesTo(s.joinStage)
-          s.trueStages = List()
-          s.falseStages = List()
-          s.addEdge(newOutEdge)
-        }
+      //Special case here, where we can merge all of the output edges into one
+      if (isTrueComb && isFalseComb) {
+        val newOutEdge = PipelineEdge(None, None, s, s.joinStage, joinStgInputs)
+        s.removeEdgesTo(s.joinStage)
+        s.addEdge(newOutEdge)
       }
       s
     case stage: SpecStage =>
@@ -72,5 +51,39 @@ object CollapseStagesPass extends StagePass[List[PStage]] {
   private def addCondStmts(stg: PStage, cond: Expr, stmts: Iterable[Command]): Unit = {
     val condstmts = stmts.map(s => ICondCommand(cond, s))
     stg.cmds = stg.cmds ++ condstmts
+  }
+
+  /**
+   * Merge src into target, but execute it conditionally
+   * @param target
+   * @param src
+   */
+  private def mergeStages(target: PStage, src: PStage) = {
+    if (src.inEdges.exists(e => e.from != target) ||
+      !src.inEdges.exists(e => e.from == target)) {
+      throw new RuntimeException(s"Cannot merge ${src.name.v} since it has inputs other than ${target.name.v}")
+    }
+    val existingedges = target.outEdges.filter(e => e.to == src)
+    if (existingedges.size > 1) {
+      throw new RuntimeException(s"Cannot merge ${src.name.v} into ${target.name.v} since they have multiple edges")
+    }
+    val cond = existingedges.head.condSend
+    //merge in the commands
+    if (cond.isDefined) {
+      addCondStmts(target, cond.get, src.cmds)
+    } else {
+      target.cmds = target.cmds ++ src.cmds
+    }
+    //merge in the output edge from src with target as the new from stage
+    //use the old condsend to src as the new condsend
+    target.removeEdgesTo(src)
+    val outedges = src.outEdges
+    val outdests = outedges.foldLeft(Set[PStage]())((s, e) => s + e.to)
+    outdests.foreach(dest => src.removeEdgesTo(dest))
+    outedges.foreach(e => {
+      val newedge = PipelineEdge(cond, e.condRecv, target, e.to, e.values)
+      target.addEdge(newedge)
+    })
+
   }
 }
