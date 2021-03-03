d1ddc5c3203b53039978192e86fd9606d6544dbe
dz333 <dz333>
2021-03-02 21:30:25 -0500

    Didn't intersect the right sets -> had to pick the values both "needed by successor" and "available in predecessor"

--- src/main/scala/pipedsl/passes/AddEdgeValuePass.scala
+++ src/main/scala/pipedsl/passes/AddEdgeValuePass.scala
@@ -17,10 +17,7 @@ object AddEdgeValuePass extends StagePass[List[PStage]] {
   override def run(stgs: List[PStage]): List[PStage] = {
     val (usedIns, _) = worklist(flattenStageList(stgs), UsedInLaterStages)
     val (_, canSendOut) = worklist(flattenStageList(stgs), CanSendToLaterStages)
-    val variablesToSend = (usedIns map { case (stg, vars) => {
-      (stg, vars.intersect(canSendOut(stg)))
-    }}).withDefaultValue(Set()) //need withdefaultvalue since that isn't preserved
-    stgs.foreach(s => addEdgeValues(s, variablesToSend, Set[Id]()))
+    stgs.foreach(s => addEdgeValues(s, usedIns, canSendOut, Set[Id]()))
     stgs
   }
 
@@ -37,35 +34,37 @@ object AddEdgeValuePass extends StagePass[List[PStage]] {
    *
    * @param stg The stage whose outgoing edges we will modify
    * @param usedIns The result of a live variable analysis
+   * @param canSend The result of a defined variable analysis - this is used to
+   *                avoid adding "false dependencies" to edges (which result because we do not use phi nodes)
    * @param dontSends The set of variables which we must not add to edges even if the analysis found them live
    *                  because they are being sent elsewhere.
    */
-  private def addEdgeValues(stg: PStage, usedIns: DFMap[Set[Id]], dontSends: Set[Id]): Unit = {
+  private def addEdgeValues(stg: PStage, usedIns: DFMap[Set[Id]], canSend: DFMap[Set[Id]], dontSends: Set[Id]): Unit = {
     stg match {
       case s: IfStage =>
         val choiceEdge = PipelineEdge(None, None, s, s.joinStage, Set(s.condVar.id))
         val newOutEdges = s.outEdges.map(e => {
-          addValues(usedIns(e.to.name), e)
+          addValues(usedIns(e.to.name).diff(dontSends).intersect(canSend(s.name)), e)
         })
         s.setEdges(s.inEdges ++ newOutEdges + choiceEdge)
-        s.condStages.foreach(sc => sc.foreach(stg => addEdgeValues(stg, usedIns, dontSends + s.condVar.id)))
-        s.defaultStages.foreach(sf => addEdgeValues(sf, usedIns, dontSends + s.condVar.id))
+        s.condStages.foreach(sc => sc.foreach(stg => addEdgeValues(stg, usedIns, canSend, dontSends + s.condVar.id)))
+        s.defaultStages.foreach(sf => addEdgeValues(sf, usedIns, canSend, dontSends + s.condVar.id))
       case s: SpecStage =>
         //Split input of join stage into two sets of inputs to be expected
         //current split algo is send everything from verify, except outputs only produced by spec
-
+        //TODO this is all made up
         val producedBySpec = usedIns(s.joinStage.name) -- usedIns(s.specStages.head.name)
         val usedSpecWorklist = Analysis(isForward = false, producedBySpec, mergeUsedVars, transferUsedVars)
         val (specUsedIns, _) = worklist(flattenStageList(s.specStages), usedSpecWorklist)
         val specUsedInsJoin = specUsedIns + (s.joinStage.name -> producedBySpec)
         //add edge values in the context where the join only expects certain values
-        s.specStages.foreach(st => addEdgeValues(st, specUsedInsJoin, dontSends))
+        s.specStages.foreach(st => addEdgeValues(st, specUsedInsJoin, canSend, dontSends))
 
         val producedByVerif = usedIns(s.joinStage.name) -- producedBySpec
         val usedVerifWorklist = Analysis(isForward = false, producedByVerif, mergeUsedVars, transferUsedVars)
         val (verifUsedIns, _) = worklist(flattenStageList(s.verifyStages), usedVerifWorklist)
         val verifUsedInsJoin = verifUsedIns + (s.joinStage.name -> producedByVerif)
-        s.verifyStages.foreach(st => addEdgeValues(st, verifUsedInsJoin, dontSends))
+        s.verifyStages.foreach(st => addEdgeValues(st, verifUsedInsJoin, canSend, dontSends))
 
         //add edges to beginning of spec section
         val specEdge = PipelineEdge(None, None, s, s.specStages.head, specUsedIns(s.specStages.head.name))
@@ -74,7 +73,8 @@ object AddEdgeValuePass extends StagePass[List[PStage]] {
       case _ =>
         stg.setEdges(stg.outEdges.map(edge => {
           //send everything except for the dontSends
-          PipelineEdge(edge.condSend, edge.condRecv, edge.from, edge.to, usedIns(edge.to.name).diff(dontSends))
+          val tosend = usedIns(edge.to.name).diff(dontSends).intersect(canSend(stg.name))
+          PipelineEdge(edge.condSend, edge.condRecv, edge.from, edge.to, tosend)
         }) ++ stg.inEdges)
     }
   }
