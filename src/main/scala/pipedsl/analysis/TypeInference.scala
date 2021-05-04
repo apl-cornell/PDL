56cd903f89ecb6d9af2182287e1f3f86dd7b0214
Kevin Zhang <kevinjianzhang@gmail.com>
2021-05-04 13:04:59 -0400

    reeady for testing

--- src/main/scala/pipedsl/analysis/TypeInference.scala
+++ src/main/scala/pipedsl/analysis/TypeInference.scala
@@ -1,12 +1,11 @@
 package pipedsl.analysis
 
-import pipedsl.common.Errors.{UnexpectedType, UnificationError}
+import pipedsl.common.Errors.{ArgLengthMismatch, UnexpectedSubtype, UnexpectedType, UnificationError}
 import pipedsl.common.Syntax._
-import pipedsl.typechecker.Subtypes.areEqual
+import pipedsl.typechecker.Subtypes.{areEqual, isSubtype}
 import pipedsl.common.Errors
-import java.util.function.ToIntFunction
-import pipedsl.typechecker.Environments.TypeEnv
-import pipedsl.typechecker.Environments
+import pipedsl.common.Syntax.Latency.{Asynchronous, Combinational, Sequential}
+import pipedsl.typechecker.Environments.{Environment, TypeEnv}
 
 object TypeInference {
   
@@ -14,32 +13,99 @@ object TypeInference {
   
   var counter = 0
 
-  def checkProgram(p: Prog) = {
-    var runningEnv: TypeEnv = TypeEnv()
-    for (f <- p.fdefs) {
-      runningEnv = runningEnv.add(f.name, TFun(f.args.map(p => p.typ), f.ret))
-      checkCommand(f.body, runningEnv.asInstanceOf[TypeEnv], List())
-    }
-    for (m <- p.moddefs) {
-      val inputTypes = m.inputs.map(p => p.typ)
-      val modTypes = m.modules.map(m => replaceNamedType(m.typ, runningEnv))
-      runningEnv = runningEnv.add(m.name, TModType(inputTypes, modTypes, m.ret, Some(m.name)))
-      checkCommand(m.body, runningEnv.asInstanceOf[TypeEnv], List())
-    }
+  def checkProgram(p: Prog): Environment[Id, Type] = {
+    val funcEnvs = p.fdefs.foldLeft[Environment[Id, Type]](TypeEnv())((env, f) => checkFunc(f, env.asInstanceOf[TypeEnv]))
+    val modEnvs = p.moddefs.foldLeft[Environment[Id, Type]](funcEnvs)((env, m) => checkModule(m, env.asInstanceOf[TypeEnv]))
+    checkCircuit(p.circ, modEnvs)
   }
 
-  def checkModule(m: ModuleDef, env: TypeEnv) = {
+  def checkCircuit(c: Circuit, tenv: Environment[Id, Type]): Environment[Id, Type] = c match {
+    case CirSeq(c1, c2) => {
+      val e1 = checkCircuit(c1, tenv)
+      checkCircuit(c2, e1)
+    }
+    case CirConnect(name, c) =>
+      val (t, env2) = checkCirExpr(c, tenv)
+      env2.add(name, t)
+    case CirExprStmt(ce) => checkCirExpr(ce, tenv)._2
+  }
+
+  private def checkCirExpr(c: CirExpr, tenv: Environment[Id, Type]): (Type, Environment[Id, Type]) = c match {
+    case CirMem(elemTyp, addrSize) => {
+      val mtyp = TMemType(elemTyp, addrSize, Asynchronous, Asynchronous)
+      (mtyp, tenv)
+    }
+    case CirRegFile(elemTyp, addrSize) => {
+      val mtyp = TMemType(elemTyp, addrSize, Combinational, Sequential)
+      (mtyp, tenv)
+    }
+    case CirNew(mod, mods) => {
+      val mtyp = tenv(mod)
+      mtyp match {
+        case TModType(_, refs, _, _) => {
+          if(refs.length != mods.length) {
+            throw ArgLengthMismatch(c.pos, mods.length, refs.length)
+          }
+          refs.zip(mods).foreach {
+            case (reftyp, mname) => {
+              if (!(isSubtype(tenv(mname), reftyp))) {
+                throw UnexpectedSubtype(mname.pos, mname.toString, reftyp, tenv(mname))
+              }
+            }
+          }
+          (mtyp, tenv)
+        }
+        case x => throw UnexpectedType(c.pos, c.toString, "Module Type", x)
+      }
+    }
+    case CirCall(mod, inits) => {
+      val mtyp = tenv(mod)
+      mtyp match {
+        case TModType(ityps, _, _, _) => {
+          if(ityps.length != inits.length) {
+            throw ArgLengthMismatch(c.pos, inits.length, ityps.length)
+          }
+          ityps.zip(inits).foreach {
+            case (expectedT, arg) => {
+              val (subst, atyp, aenv) = infer(tenv.asInstanceOf[TypeEnv], arg)
+              if (!isSubtype(atyp, expectedT)) {
+                throw UnexpectedSubtype(arg.pos, arg.toString, expectedT, atyp)
+              }
+            }
+          }
+          (mtyp, tenv)
+        }
+        case x => throw UnexpectedType(c.pos, c.toString, "Module Type", x)
+      }
+    }
+  }
+  
+  def checkModule(m: ModuleDef, env: TypeEnv): Environment[Id, Type] = {
     val inputTypes = m.inputs.map(p => p.typ)
     val modTypes = m.modules.map(m => replaceNamedType(m.typ, env))
     val modEnv = env.add(m.name, TModType(inputTypes, modTypes, m.ret, Some(m.name)))
-    for ()
+    val inEnv = m.inputs.foldLeft[Environment[Id, Type]](modEnv)((env, p) => env.add(p.name, p.typ))
+    val pipeEnv = m.modules.foldLeft[Environment[Id, Type]](inEnv)((env, m) => env.add(m.name, m.typ))
+    checkCommand(m.body, pipeEnv.asInstanceOf[TypeEnv], List())
+    modEnv
+  }
+  
+  def checkFunc(f: FuncDef, env: TypeEnv): Environment[Id, Type] = {
+    val inputTypes = f.args.map(a => a.typ)
+    val funType = TFun(inputTypes, f.ret)
+    val funEnv = env.add(f.name, funType)
+    val inEnv = f.args.foldLeft[Environment[Id, Type]](funEnv)((env, a) => env.add(a.name, a.typ))
+    checkCommand(f.body, inEnv.asInstanceOf[TypeEnv], List())
+    funEnv
   }
 
   private def replaceNamedType(t: Type, tenv: TypeEnv): Type = t match {
     case TNamedType(name) => tenv(name)
     case _ => t
   }
-
+//INVARIANTS
+//Transforms the argument sub by composing any additional substitution
+//Transforms the argument env by subbing in the returned substitution and adding any relevatn variables
   def checkCommand(c: Command, env: TypeEnv, sub: Subst): (TypeEnv, Subst) = c match {
     case CLockOp(mem, op) =>  //test basic first
       (env, sub)
@@ -48,36 +114,37 @@ object TypeInference {
     case CLockStart(mod) => throw new RuntimeException()//how to unify or condition?
     case CIf(cond, cons, alt) =>
       //TODO: Might need to do some special handling with composing substitutions in this case
-      val (condS, condT) = infer(env, cond)
-      val newSub = compose_subst(sub, condS)
-      val newEnv = env.apply_subst_typeenv(condS)
+      val (condS, condT, env1) = infer(env, cond)
+      val tempSub = compose_subst(sub, condS)
+      val condTyp = apply_subst_typ(tempSub, condT)
+      val newSub = compose_subst(tempSub, unify(condTyp, TBool()))
+      val newEnv = env1.apply_subst_typeenv(newSub)
       val (consEnv, consSub) = checkCommand(cons, newEnv, newSub)
       val newEnv2 = newEnv.apply_subst_typeenv(consSub)
       val (altEnv, altSub) = checkCommand(alt, newEnv2, consSub)
       //TODO: Intersection of both envs?
-      ((consEnv.apply_subst_typeenv(altSub).intersect(altEnv.apply_subst_typeenv(altSub)).asInstanceOf[TypeEnv]), altSub)
+      (consEnv.apply_subst_typeenv(altSub).intersect(altEnv).asInstanceOf[TypeEnv], altSub)
     case CLockEnd(mod) => (env, sub) //how to unify an or condition?
     case CSplit(cases, default) => //TODO 
       var (runningEnv, runningSub) = checkCommand(default, env, sub)
-      runningEnv = runningEnv.apply_subst_typeenv(runningSub)
       for (c <- cases) {
-        val (condS, condT) = infer(env, c.cond)
-        //construct substitution you will use to check command
-        val newSub = compose_subst(runningSub, condS)
+        val (condS, condT, env1) = infer(env, c.cond)
+        val tempSub = compose_subst(runningSub, condS)
+        val condTyp = apply_subst_typ(tempSub, condT)
+        val newSub = compose_subst(tempSub, unify(condTyp, TBool()))
         //apply substitution to original environmnet, which you will use to check the body
         val newEnv = env.apply_subst_typeenv(newSub)
         val (caseEnv, caseSub) = checkCommand(c.body, newEnv, newSub)
         runningSub = caseSub
-        runningEnv = runningEnv.apply_subst_typeenv(runningSub).intersect(caseEnv.apply_subst_typeenv(runningSub))
+        runningEnv = runningEnv.apply_subst_typeenv(runningSub).intersect(caseEnv).asInstanceOf[TypeEnv]
       }
       (runningEnv, runningSub)
     case CExpr(exp) => (env, sub) //TODO
     case CCheck(predVar) => (env, sub)
     case CTBar(c1, c2) =>
       val (e, s) = checkCommand(c1, env, sub)
-      val e1 = e.apply_subst_typeenv(s)
-      val (e2, s2) = checkCommand(c2, e1, s)
-      (e2.apply_subst_typeenv(s2), s2)
+      val (e2, s2) = checkCommand(c2, e, s)
+      (e2, s2)
     case CPrint(evar) => (env, sub)
     //how to unify or conditions> This I guess is an polymorphic function with restrictions
     case CSpeculate(predVar, predVal, verify, body) => (env, sub)
@@ -85,36 +152,41 @@ object TypeInference {
       (env, sub)
       //How to check wellformedness with the module body
     case CRecv(lhs, rhs, typ) =>
-      val (slhs, tlhs) = infer(env, lhs)
-      val (srhs, trhs) = infer(env, rhs)
-      val s1 = unify(tlhs, trhs)
-      val sret = compose_many_subst(sub, slhs, srhs, s1, typ match {
-        case Some(value) => compose_subst(unify(tlhs, value), unify(trhs, value))
+      val (slhs, tlhs, lhsEnv) = infer(env, lhs)
+      val (srhs, trhs, rhsEnv) = infer(lhsEnv, rhs)
+      val tempSub = compose_many_subst(sub, slhs, srhs)
+      val lhstyp = apply_subst_typ(tempSub, tlhs)
+      val rhstyp = apply_subst_typ(tempSub, trhs)
+      val s1 = unify(lhstyp, rhstyp)
+      val sret = compose_many_subst(tempSub, s1, typ match {
+        case Some(value) => compose_subst(unify(lhstyp, value), unify(rhstyp, value))
         case None => List()
       })
       val newEnv = lhs match {
-        case EVar(id) => env.add(id, tlhs)
-        case _ => env
+        case EVar(id) => rhsEnv.add(id, tlhs)
+        case _ => rhsEnv
       }
       (newEnv.asInstanceOf[TypeEnv].apply_subst_typeenv(sret), sret)
     case CAssign(lhs, rhs, typ) =>
-      val (slhs, tlhs) = infer(env, lhs)
-      val (srhs, trhs) = infer(env, rhs)
-      val s1 = unify(tlhs, trhs)
-      val sret = compose_many_subst(sub, slhs, srhs, s1, typ match {
-        case Some(value) => compose_subst(unify(tlhs, value), unify(trhs, value))
+      val (slhs, tlhs, lhsEnv) = infer(env, lhs)
+      val (srhs, trhs, rhsEnv) = infer(lhsEnv, rhs)
+      val tempSub = compose_many_subst(sub, slhs, srhs)
+      val lhstyp = apply_subst_typ(tempSub, tlhs)
+      val rhstyp = apply_subst_typ(tempSub, trhs)
+      val s1 = unify(lhstyp, rhstyp)
+      val sret = compose_many_subst(tempSub, s1, typ match {
+        case Some(value) => compose_subst(unify(lhstyp, value), unify(rhstyp, value))
         case None => List()
       })
       val newEnv = lhs match {
-        case EVar(id) => env.add(id, tlhs)
-        case _ => env
+        case EVar(id) => rhsEnv.add(id, tlhs)
+        case _ => rhsEnv
       }
       (newEnv.asInstanceOf[TypeEnv].apply_subst_typeenv(sret), sret)
     case CSeq(c1, c2) => 
-      val (e, s) = checkCommand(c1, env, sub)
-      val e1 = e.apply_subst_typeenv(s)
+      val (e1, s) = checkCommand(c1, env, sub)
       val (e2, s2) = checkCommand(c2, e1, s)
-      (e2.apply_subst_typeenv(s2), s2)
+      (e2, s2)
   }
 
   private def generateTypeVar(): TNamedType = TNamedType(Id("__TYPE__" + counter)); counter += 1
@@ -189,59 +261,98 @@ object TypeInference {
     case _ => throw UnificationError(a, b)
   }
   
-  //Updating the type environment with the new subtitution whenever you generate one allows errors to be found :D
-  private def infer(env: TypeEnv, e: Expr): (Subst, Type) = e match {
-    case EInt(v, base, bits) => (List(), TSizedInt(bits, true))
-    case EString(v) => (List(), TString())
-    case EBool(v) => (List(), TBool())
+  //Updating the type environment with the new substitution whenever you generate one allows errors to be found :D
+  //The environment returned is guaratneed to already have been substituted into with the returned substitution
+  private def infer(env: TypeEnv, e: Expr): (Subst, Type, TypeEnv) = e match {
+    case EInt(v, base, bits) => (List(), TSizedInt(bits, true), env)
+    case EString(v) => (List(), TString(), env)
+    case EBool(v) => (List(), TBool(), env)
     case EUop(op, ex) => 
-      val (s, t) = infer(env, ex)
+      val (s, t, env1) = infer(env, ex)
       val retType = generateTypeVar()
-      val subst = unify(TFun(List(t), retType), uOpExpectedType(op))
-      (compose_subst(s, subst), retType)
+      val tNew = apply_subst_typ(s, t)
+      val subst = unify(TFun(List(tNew), retType), uOpExpectedType(op))
+      val retSubst = compose_subst(s, subst)
+      val retTyp = apply_subst_typ(retSubst, retType)
+      (retSubst, retTyp, env1.apply_subst_typeenv(retSubst))
     case EBinop(op, e1, e2) =>
-      val (s1, t1) = infer(env, e1) 
-      val newEnv = env.apply_subst_typeenv(s1)
-      val (s2, t2) = infer(newEnv, e2)
+      val (s1, t1, env1) = infer(env, e1)
+      val (s2, t2, env2) = infer(env1, e2)
       val retType = generateTypeVar()
-      val subst = unify(TFun(List(t2,t1), retType), binOpExpectedType(op))
-      (compose_many_subst(s1, s2, subst), retType)
+      val subTemp = compose_subst(s1, s2)
+      val t1New = apply_subst_typ(subTemp, t1)
+      val t2New = apply_subst_typ(subTemp, t2)
+      val subst = unify(TFun(List(t2New,t1New), retType), binOpExpectedType(op))
+      val retSubst = compose_subst(subTemp, subst)
+      val retTyp = apply_subst_typ(retSubst, retType)
+      (retSubst, retTyp, env2.apply_subst_typeenv(retSubst))
     case EMemAccess(mem, index) => 
       if(!env(mem).isInstanceOf[TMemType]) throw UnexpectedType(e.pos, "Memory Access", "TMemtype", env(mem))
-      val retTyp = generateTypeVar()
-      val (s, t) = infer(env, index)
-      val subst = unify(TFun(List(t), retTyp), getMemAccessType(env(mem).asInstanceOf[TMemType]))
-      (compose_subst(s,subst), retTyp)
+      val retType = generateTypeVar()
+      val (s, t, env1) = infer(env, index)
+      val tTemp = apply_subst_typ(s, t)
+      val subst = unify(TFun(List(tTemp), retType), getMemAccessType(env1(mem).asInstanceOf[TMemType]))
+      val retSubst = compose_subst(s,subst)
+      val retTyp = apply_subst_typ(retSubst, tTemp)
+      (retSubst, retTyp, env1.apply_subst_typeenv(retSubst))
     case EBitExtract(num, start, end) => throw new RuntimeException() //TODO
     case ETernary(cond, tval, fval) =>  
-      val (sc, tc) = infer(env, cond)
-      val env1 = env.apply_subst_typeenv(sc)
-      val (st, tt) = infer(env1, tval)
-      val env2 = env1.apply_subst_typeenv(st)
-      val (sf, tf) = infer(env, fval)
-      val substc = unify(tc, TBool())
-      val subst = unify(tt, tf)
-      (compose_many_subst(sc, st, sf, substc, subst), tt)
+      val (sc, tc, env1) = infer(env, cond)
+      val (st, tt, env2) = infer(env1, tval)
+      val (sf, tf, env3) = infer(env2, fval)
+      val substSoFar =  compose_many_subst(sc, st, sf)
+      val tcNew = apply_subst_typ(substSoFar, tc)
+      val ttNew = apply_subst_typ(substSoFar, tt)
+      val tfNew = apply_subst_typ(substSoFar, tf)
+      val substc = unify(tcNew, TBool())
+      val subst = unify(ttNew, tfNew)
+      val retSubst = compose_many_subst(sc, st, sf, substc, subst)
+      val retType = apply_subst_typ(retSubst, ttNew)
+      (retSubst, retType, env3.apply_subst_typeenv(retSubst))
     case EApp(func, args) => 
       val expectedType = env(func)
       val retType = generateTypeVar()
-      val (subList, typeList) = args.map(a => infer(env, a)).unzip
+      var runningEnv: TypeEnv = env
+      var runningSubst: Subst = List()
+      var typeList: List[Type] = List()
+      for (a <- args) {
+        val (sub, typ, env1) = infer(runningEnv, a) 
+        runningSubst = compose_subst(runningSubst, sub)
+        typeList = typeList :+ typ
+        runningEnv = env1
+      }
+      typeList = typeList.map(t=> apply_subst_typ(runningSubst, t))
       val subst = unify(TFun(typeList, retType), expectedType)
-      (compose_many_subst((subList :+ subst).toSeq:_*), retType)
+      val retSubst = compose_subst(runningSubst, subst)
+      val retEnv = runningEnv.apply_subst_typeenv(retSubst)
+      val retTyp = apply_subst_typ(retSubst, retType)
+      (retSubst, retTyp, retEnv)
     case ECall(mod, args) => 
       if (!env(mod).isInstanceOf[TModType]) throw UnexpectedType(e.pos, "Module Call", "TModType", env(mod))
       val expectedType = getArrowModType(env(mod).asInstanceOf[TModType])
       val retType = generateTypeVar()
-      val (subList, typeList) = args.map(a => infer(env, a)).unzip
+      var runningEnv: TypeEnv = env
+      var runningSubst: Subst = List()
+      var typeList: List[Type] = List()
+      for (a <- args) {
+        val (sub, typ, env1) = infer(runningEnv, a)
+        runningSubst = compose_subst(runningSubst, sub)
+        typeList = typeList :+ typ
+        runningEnv = env1
+      }
+      typeList = typeList.map(t=> apply_subst_typ(runningSubst, t))
       val subst = unify(TFun(typeList, retType), expectedType)
-      (compose_many_subst((subList :+ subst).toSeq:_*), retType)
-    case EVar(id) => (List(), env(id))
+      val retSubst = compose_subst(runningSubst, subst)
+      val retEnv = runningEnv.apply_subst_typeenv(retSubst)
+      val retTyp = apply_subst_typ(retSubst, retType)
+      (retSubst, retTyp, retEnv)
+    case EVar(id) => (List(), env(id), env)
     case ECast(ctyp, exp) => 
       //TODO this is wrong probably
-      val (s, t) = infer(env, exp) 
+      val (s, t, env1) = infer(env, exp) 
       val newT = apply_subst_typ(s, t)
       if (!areEqual(ctyp, newT)) throw Errors.IllegalCast(e.pos, ctyp, newT)
-      (s, ctyp)
+      (s, ctyp, env1)
   }
   
   private def binOpExpectedType(b: BOp): Type = b match {
