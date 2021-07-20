f90d92a30b054206a5304a6c26e795d9991d7d1d
Charles Sherk <cs897@cornell.edu>
2021-07-20 11:34:49 -0400

    update build script for runnign ECELinux tools

--- build_tmp.sh
+++ build_tmp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 find tmp/Circuit_verilog/ -type f -not -name "mkTB.v" -exec cat {} \; |
-    cat - ~/bsc/src/Verilog/*.v |
-    sed 's/CLK/clk/g' - > tmp/Circuit__pickled.v
+    cat - bscRuntime/verilog/*.v |
+    cat - ~/bsc/src/Verilog/RevertReg.v ~/bsc/src/Verilog/FIFO2.v ~/bsc/src/Verilog/SizedFIFO.v |
+    sed 's/CLK/clk/g' - > tmp/mkCircuit__pickled.v
